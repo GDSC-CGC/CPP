ddc9a90ee26ad4fc3e6a4a123ee25685b0bc1399
harshil sharma <harshilsharmaa0@gmail.com>
2022-10-08 11:49:08 +0530

    Task2 2002848

---
+++ Submissions/2002848_task2/task2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+
+    cout<<"Size of char: "<<sizeof(char)<<endl;
+    cout<<"Size of int: "<<sizeof(int)<<endl;
+    cout<<"Size of float: "<<sizeof(float)<<endl;
+    cout<<"Size of double: "<<sizeof(double)<<endl;
+    cout<<"Size of long int: "<<sizeof(long int)<<endl;
+ 
+   return 0;
+}
