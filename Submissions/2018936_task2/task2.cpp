d3c2efc8f250c2ec42819dc0fafee7b125b45655
Kgaurav2002 <82453526+Kgaurav2002@users.noreply.github.com>
2022-10-11 22:17:39 +0530

    Create task2.cpp

---
+++ Submissions/2018936_task2/task2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// program to print the exact size of 5 data types
+   int number;
+   char name;
+   float num;
+   double count;
+  bool flag;
+   cout<<sizeof(number)<<endl;
+   cout<<sizeof(name)<<endl;
+   cout<<sizeof(num)<<endl;
+   cout<<sizeof(count)<<endl;
+  cout<<sizeof(flag)<<endl;
+	return 0;
+}
