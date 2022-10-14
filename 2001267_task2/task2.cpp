fab87d5fbd941e39918359f46d0176d4304bb8fb
Mayank241202 <96975833+Mayank241202@users.noreply.github.com>
2022-10-14 17:44:10 +0530

    Add files via upload

---
+++ 2001267_task2/task2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+
+    cout<<"Size of integer is: "<<sizeof(int)<<endl;
+    cout<<"Size of float is: "<<sizeof(float)<<endl;
+    cout<<"Size of character is: "<<sizeof(char)<<endl;
+    cout<<"Size of long int is: "<<sizeof(long int)<<endl;
+    cout<<"Size of double is: "<<sizeof(double)<<endl;
+   
+   return 0;
+
+}
