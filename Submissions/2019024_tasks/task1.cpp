1d136506f461d210503f3dd2d8e307b5b31af6d7
Shailika <90005665+coder-000@users.noreply.github.com>
2022-10-17 10:35:42 +0530

    task1_completed

---
+++ Submissions/2019024_tasks/task1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+    cout <<"Hello! Google Developers Club."; // prints Hello World
+    return 0;
+}
