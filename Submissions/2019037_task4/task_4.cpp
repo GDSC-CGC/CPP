23fe0a59a11305ecae882c7c63e15a3be34a65b9
shubham05116 <88928071+shubham05116@users.noreply.github.com>
2022-10-19 09:42:49 +0530

    Task 4 submitted

---
+++ Submissions/2019037_task4/task_4.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int sumOfNumbers(int num) 
+
+{
+
+  int sum = 0;
+
+  
+
+  for (int i = 1; i <= num; i++)
+
+  {
+
+    //calculating the sun
+
+    sum = sum + i;
+
+  }
+
+  
+
+  //returning the total sum till n
+
+  return sum;
+
+  
+
+}
+
+int main() 
+
+{
+
+  int number;
+
+  
+
+  //taking input of the number
+
+  cin>>number;
+
+  
+
+  //calling the function 
+
+  //after the execution of the function, it will display output
+
+  cout<<sumOfNumbers(number);
+
+  
+
+  return 0;
+
+}
