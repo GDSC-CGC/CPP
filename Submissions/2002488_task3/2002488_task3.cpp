bdab54e7486fe21ffd543b2278b56daf222f8258
Keshav Kumar <71294563+dev-keshav@users.noreply.github.com>
2022-10-21 01:03:31 +0530

    2002488_task3.cpp

---
+++ Submissions/2002488_task3/2002488_task3.cpp
@@ -0,0 +1,20 @@
+#includeiostream
+using namespace std;
+ 
+int main(){
+ 
+    int n = 5;
+    
+    for(int i=1;i=5;i++){
+        
+        for(int k=0;kn-i;k++){
+            cout ;
+        }
+        for(int j=0;ji;j++){
+            cout ;
+        }
+        coutendl;
+    }
+
+   return 0;
+}
