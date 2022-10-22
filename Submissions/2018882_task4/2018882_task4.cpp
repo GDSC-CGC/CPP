c435268d59ab549579d26f0c3414ef90e2cc9a03
Deepanshugverma <116455047+Deepanshugverma@users.noreply.github.com>
2022-10-23 00:41:55 +0530

    Create 2018882_task4.cpp

---
+++ Submissions/2018882_task4/2018882_task4.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum=0;
+    cin>>n;
+    for(int i=1;i<n;i++){
+        sum+=i;
+    }
+    cout<<"Sum of "<<n<<" numbers are: "<<sum;
+    return 0;
+}
