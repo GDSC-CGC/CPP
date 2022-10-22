9b44c4f03aa0b7ec093189286beabbc81f69b92d
Prathamahuja <114613629+Prathamahuja@users.noreply.github.com>
2022-10-22 14:10:07 +0530

    Create 2121410_task3.cpp

---
+++ Submissions/2121410_task3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int e = 1;
+
+    cout << " Print the traingle pattern using stars " ; 
+
+    for (int a = 1; a <= 6; a++)
+    {
+        //using for loop 
+        for (int b = 5 ;  b >= a; b--)
+        {
+            cout << " "; 
+        }
+        for (int c = 0; c < i; c++)
+        {
+            cout << "*"; 
+        }
+        cout << endl; 
+        e = e + 2;
+    }
+}
