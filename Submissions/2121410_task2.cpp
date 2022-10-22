817c1a4635a86f7a4a0786d2d7fa17c79b0fcf28
Prathamahuja <114613629+Prathamahuja@users.noreply.github.com>
2022-10-22 14:07:09 +0530

    Create 2121410_task2.cpp

---
+++ Submissions/2121410_task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    cout << "the Size of char : " << sizeof(char) << " byte" << endl; //char
+    cout << "the Size of int: " << sizeof(int) << " bytes" << endl;   //int 
+    cout << "the Size of float: " << sizeof(float) << " bytes" << endl; //float
+    cout << "the Size of double: " << sizeof(double) << " bytes" << endl;  //double
+
+    return 0;
+}
