63586bf56820081cfb5ac1f49c9824d0207f95cb
VanshikaChanana29 <71041158+VanshikaChanana29@users.noreply.github.com>
2022-10-22 11:14:40 +0530

    Create 2121513_task2.cpp

---
+++ Submissions/2121513_task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    cout << "the Size of char : " << sizeof(char) << " byte" << endl;
+    cout << "the Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "the Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "the Size of double: " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
