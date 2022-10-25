e06a0197368589ecd3d8b11d4d7556b63adf0f86
Samarth Chawla <93388133+samarth2209@users.noreply.github.com>
2022-10-26 01:08:20 +0530

    Create 2002613-task4.cpp

---
+++ 2002613_task4/2002613-task4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void returnSum(int n){
+	cout<<n*(n+1)/2;
+}
+int main(){
+	int n;
+	cout<<"Enter n:";
+	cin>>n;
+    cout<<"Sum is:";
+	return Sum(n);
+	return 0;
+}
